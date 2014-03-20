6678ba3347b249c28cac0355d785bdb5019b5c73
saml <skynare@gmail.com>
2014-03-20 18:17:10 -0400

    adding manage

--- manage.py
+++ manage.py
@@ -1,2 +1,15 @@
 from flask_script import Shell, Manager, Server
 
+from liwebl import create_app
+from liwebl.extensions import db
+
+app = create_app()
+manager = Manager(app)
+
+def _make_context():
+    return dict(app=app, db=db)
+    
+if __name__ == '__main__':
+    manager.add_command('runserver', Server())
+    manager.add_command('shell', Shell(make_context=_make_context))
+    manager.run()
